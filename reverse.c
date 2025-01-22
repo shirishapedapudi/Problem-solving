85e1eb249102153d40b95dd7d0b785ed6ca86062
shirishapedapudi <147227421+shirishapedapudi@users.noreply.github.com>
2025-01-22 11:56:05 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n,rev=0,rem;
+    printf("enter the number:");
+    scanf("%d",&n);
+    int temp=n;
+    while(n!=0){
+        rem=n%10;
+        rev=rev*10+rem;
+        n/=10;
+    }
+    printf("reverse number:%d\n",rev);
+
+    return 0;
+}
